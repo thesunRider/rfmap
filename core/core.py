7e38a7bbf98ce3cd69c85e34e44af9f7c7b416b2
thesunRider <suryasaradhi3@gmail.com>
2022-12-24 03:25:28 +0530

    Added core

---
+++ core/core.py
@@ -0,0 +1,36 @@
+
+import logging
+import sys,json,re
+import time,glob,datetime
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+config_file = 'config.json'
+config_file_handle = open(config_file)
+config = json.load(config_file_handle)
+
+def __getsave_model(model_id):
+	timestr = time.strftime("%Y%m%d-%H%M%S")
+	return os.path.abspath(config['weight_path']) +'/'+ str(model_id) +':'+ timestr +'.hd5'
+
+def __listsave_model(model_id):
+	return glob.glob(os.path.abspath(config['weight_path']) +'/'+str(model_id)+':*.hd5')
+
+
+def __getlatest_model(model_id):
+	file_list = __listsave_model(model_id)
+	date_ary = []
+	for x in file_list:
+		file_stamp = ____extract_timestamp(x)
+		date_ary.append(datetime.datetime.strptime(file_stamp, "%Y%m%d-%H%M%S"))
+
+	assert date_ary #if no model file was found then raise assertion error
+	return os.path.abspath(config['weight_path']) +'/'+ str(model_id) +':'+ str(sorted(date_ary)[-1].strftime("%Y%m%d-%H%M%S"))+'.hd5'
+
+
+def ____extract_timestamp(filename):
+    return re.findall(re.escape(':')+"(.*)"+re.escape('.hd5'),filename)[0]
+
+
---
+++ core/net_model.py
@@ -0,0 +1,106 @@
+import sigmf
+import numpy as np
+import pandas as pd
+from sigmf import SigMFFile, sigmffile
+import os
+import glob,json
+import re,pickle
+
+import matplotlib.pyplot as plt
+
+#importing the required libraries
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPool2D
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dropout,Input
+from tensorflow.keras.layers import Dense,BatchNormalization
+from tensorflow.keras import regularizers
+from numpy.fft import fft, ifft,fftshift,ifftshift
+
+from imblearn.over_sampling import SMOTE
+from scipy.stats import wasserstein_distance #this is EMD
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from scipy.stats import multivariate_normal
+from sklearn import preprocessing
+from sklearn.preprocessing import MinMaxScaler 
+from scipy.signal import butter,filtfilt
+from sklearn.model_selection import train_test_split
+from scipy import signal
+
+import logging
+import sys
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+# Test messages
+#logger.debug("Harmless debug Message")
+#logger.info("Just an information")
+#logger.warning("Its a Warning")
+#logger.error("Did you try to divide by zero")
+#logger.critical("Internet is down")
+
+class Netmodel:
+	model_id = 232313123
+	model_descriptor_file = 'model_descriptor_' + str(model_id) +'.json'
+	model_descriptor_handle = open(model_descriptor_file)
+	model_descriptor = json.load(model_descriptor_handle)
+	model = None
+
+	def __init__():
+		#load configs and create model
+
+		#adding model convolution layer
+		self.model = Sequential()
+		self.model.add(Conv2D(50,(1,7),activation='relu',input_shape=(2,128,1)))
+		self.model.add(BatchNormalization())
+		self.model.add(MaxPool2D(pool_size=(2, 2),strides=1,padding="same"))
+
+		self.model.add(Conv2D(50,(2,7),activation='relu'))
+		self.model.add(BatchNormalization())
+		self.model.add(MaxPool2D(pool_size=(2, 2),strides=1,padding="same"))
+
+		self.model.add(Flatten())
+
+		self.model.add(Dense(256,kernel_regularizer=regularizers.L2(l2=1e-4) ))
+		self.model.add(Dropout(0.5))
+		self.model.add(Dense(80,kernel_regularizer=regularizers.L2(l2=1e-4) ))
+		self.model.add(Dropout(0.5))
+
+		#adding output layer
+		self.model.add(Dense(16,activation='softmax' ))
+		#compiling the model
+		self.model.compile(loss='sparse_categorical_crossentropy',optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4),metrics=['accuracy'])
+
+		return (self.model_descriptor,self.model)
+
+	def predict(x_pred):
+		return model.predict(x_pred)
+
+	def train_model(x_train,y_train,x_test,x_test,save_weights=False,**kwargs):
+		#fitting the model
+		self.model.fit(x_train,y_train,epochs=5,validation_data=(x_test, x_test),)
+		if save_weights:
+			save_file = core.__getsave_model(self.model_id)
+			logger.info("Trained Model saved to :" + save_file)
+			self.model.save_weights(save_file)
+	
+	def load_model(weight_location=None):
+		#if none weight_location,load latest saved weight
+		if weight_location:
+			self.model.load_weights(weight_location)
+		else:
+			self.model.load_weights(core.__getlatest_model(self.model_id))
+
+	def list_saved_models():
+		return core.__listsave_model(self.model_id)
+
+	def get_model():
+		return self.model
+
+
